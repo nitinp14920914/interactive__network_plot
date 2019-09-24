973aae99b67ee74d14c0fce94f7a2e20a9ba7bdd
Nitin k.chauhan <33024918+nitinp14920914@users.noreply.github.com>
2019-09-24 17:20:24 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,22 @@
+
+'''Nitin k.chauhan under Lynngroup3.0'''
+# usage $python plot.py input_edgelist_file output_file
+#output will be created as .html open it in browser
+
+#PYTHON3
+#pip install pyvis
+#pip install networkx
+
+from pyvis.network import Network
+import networkx as nx
+import sys
+
+g=Network(height=1080,width=1920) #can specify your screen resolution
+g.toggle_hide_edges_on_drag(False)
+g.barnes_hut()
+#input
+g.from_nx(nx.read_edgelist(sys.argv[1])) # convert graph component in networkx
+#output
+g.show(str(sys.argv[2])+".html")
+
+
